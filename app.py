aa28354ff1ed51c8b3aca6f0e1d53b509b007df5
AndrewHu-it <115961335+AndrewHu-it@users.noreply.github.com>
2025-02-07 22:36:02 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 # app.py
-import os
-from factory import create_app
-from config import Config
 
-app = create_app()
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Python Azure Web App! VERSION 23. This time try commiting"
 
 if __name__ == "__main__":
-    # Use the port Azure sets, or default to 8000 locally
+    # Azure typically sets PORT via an environment variable; default to 8000 locally
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
+
