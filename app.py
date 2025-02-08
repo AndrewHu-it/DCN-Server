736dce8db74d1d09141c370f0d1394b0066f4f7f
AndrewHu-it <115961335+AndrewHu-it@users.noreply.github.com>
2025-02-08 13:12:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ def index():
     return "Hello from the single-file Python Azure Web App!"
 
 if __name__ == "__main__":
-    port = 8080
+    port = 8000
     # Run the application on the specified host and port
     app.run(host="0.0.0.0", port=port)
