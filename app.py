c6351490997e9336d332cdac1f8ef7876ab98f88
andrew <ahurlbut999@gmail.com>
2025-02-14 08:05:30 -0800

    added app.py

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-from flask import Flask
 
-# Initialize the Flask application
+from flask import Flask
+from routes.getinfo import *
+
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    """
-    A simple route that returns a text response.
-    """
-    return "Hello from the single-file Python Azure Web App!"
-
-if __name__ == "__main__":
-    port = 8080
-    # Run the application on the specified host and port
-    app.run(host="0.0.0.0", port=port)
+# main driver function
+if __name__ == '__main__':
+    app.run()
