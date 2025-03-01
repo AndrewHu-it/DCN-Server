7558c248204538b66bb68cbec28c7c7974124478
andrew <ahurlbut999@gmail.com>
2025-02-28 16:46:42 -0800

    Add dockerfile

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from app import create_app
 
+# TODO: construct a config using environment variables
+
+# TODO: pass the config into the app
 app = create_app()
 
 if __name__ == '__main__':
+    print('Starting server...')
     app.run(host='0.0.0.0', port=5000)
